54c4f3c59b86ba9baa9f53bd5c2232cd39f8a80b
Alireza <alireza_paridar@outlook.com>
2016-03-09 20:35:29 +0330

    Start

---
+++ src/TagCompressor.NetCore/Compress.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Mvc.Filters;
+using System.IO;
+using System.Text;
+
+
+namespace TagCompressor.NetCore
+{
+    public class Compress : ActionFilterAttribute
+    {
+        Stream stream;
+        MemoryStream ms = new MemoryStream();
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            stream = context.HttpContext.Response.Body;
+
+            ms = new MemoryStream();
+            context.HttpContext.Response.Body = ms;
+
+            base.OnActionExecuting(context);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+
+            // ms.Position = 0;
+
+            byte[] buf = ms.ToArray();
+
+            string html = HtmlCompressor.Compress(Encoding.ASCII.GetString(buf));
+            byte[] res = Encoding.ASCII.GetBytes(html);
+
+            stream.Position = 0;
+            stream.Write(res, 0, res.Length);
+
+        }
+    }
+}
---
+++ src/TagCompressor.NetCore/HtmlCompressor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace TagCompressor.NetCore
+{
+    class HtmlCompressor
+    {
+        public static string Compress(string html)
+        {
+            string pattern = @"\n+";
+            Regex r = new Regex(pattern);
+            string compressed = r.Replace(html, "");
+
+            pattern = @"\s+";
+            r = new Regex(pattern);
+            compressed = r.Replace(compressed, " ");
+
+
+            return compressed;
+        }
+    }
+}
